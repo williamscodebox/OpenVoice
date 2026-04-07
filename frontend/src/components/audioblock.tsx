36f5bbb3e5fb2c40b0308dbc8475b60540b412fb
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-04-06 21:12:29 -0400

    add audioblock

---
+++ frontend/src/components/audioblock.tsx
@@ -0,0 +1,26 @@
+import { useAudioRecorder } from "../utils/hooks/useAudioRecorder.ts";
+import { sendAudio } from "../utils/utils.ts";
+
+export default function AudioRecorderUI() {
+  const { startRecording, stopRecording, isRecording } = useAudioRecorder();
+
+  return (
+    <>
+      <button onClick={startRecording} disabled={isRecording}>
+        Start Recording
+      </button>
+
+      <button
+        onClick={async () => {
+          const blob = await stopRecording();
+          await sendAudio(blob);
+        }}
+        disabled={!isRecording}
+      >
+        Stop & Send
+      </button>
+    </>
+  );
+}
+
+
