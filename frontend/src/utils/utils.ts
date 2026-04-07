4187943e4580f825b747c813d4d67972794035db
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-04-06 20:57:20 -0400

    update types

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,12 +1,35 @@
 import { callBackend } from "./apiClient";
-import type { User } from "../types/types";
+import type { Result } from "../types/types";
 
-export async function main() {
+export async function audioCall() {
   try {
-    const user = await callBackend<User>("run");
+    const user = await callBackend<Result>("run");
     console.log("User:", user);
     return user.result;
   } catch (err) {
     console.error("Request failed:", err);
   }
 }
+
+const sendAudio = async (audioBlob: Blob) => {
+  const formData = new FormData();
+  // formData.append("file", audioBlob, "input.wav");
+  formData.append("file", audioBlob, "input.webm");
+
+  const res = await fetch("http://localhost:8000/process-audio", {
+    method: "POST",
+    body: formData,
+  });
+
+  const arrayBuffer = await res.arrayBuffer();
+  const blob = new Blob([arrayBuffer], { type: "audio/mpeg" });
+  const url = URL.createObjectURL(blob);
+
+  const audio = new Audio(url);
+  audio.play();
+};
+
+mediaRecorder.onstop = async () => {
+  const audioBlob = new Blob(chunks, { type: "audio/webm" });
+  await sendAudio(audioBlob);
+};
